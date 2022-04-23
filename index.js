b024c8c00f9cb9f9cb7e44b7a19e7bc6e2be527d
Uzzwal Maharjan <ujolmaharjan94@gmail.com>
2022-04-23 18:28:47 +0545

    store added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-const BOOK_ORDERED = 'BOOK_ORDERED'
+const redux = require('redux')
+const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
-const buyBook = () => {
+const BOOK_ORDERED = 'BOOK_ORDERED'
+const BOOK_RESTOCK = 'BOOK_RESTOCK'
+
+const orderBook = () => {
   return {
     type: BOOK_ORDERED,
     quantity: 10,
+  }
+}
+
+const restockBook = (qty) => {
+  return {
+    type: BOOK_RESTOCK,
+    payload: qty,
   }
 }
 
@@ -15,7 +27,30 @@ const reducer = (state = initialStale, action) => {
   switch (action.type) {
     case BOOK_ORDERED:
       return { ...state, noOfBooks: state.noOfBooks - 1 }
+    case restockBook: {
+      return { ...state, noOfBooks: state.noOfBooks + action.payload }
+    }
     default:
       return state
   }
 }
+
+const store = createStore(reducer)
+console.log('initial state', store.getState())
+const unsubscribe = store.subscribe(() =>
+  console.log('updated state', store.getState())
+)
+
+// store.dispatch(orderBook())
+// store.dispatch(orderBook())
+// store.dispatch(orderBook())
+
+// store.dispatch(restockBook(90))
+const actions = bindActionCreators({ orderBook, restockBook }, store.dispatch)
+
+actions.orderBook()
+actions.orderBook()
+actions.orderBook()
+actions.restockBook(56)
+
+unsubscribe()
